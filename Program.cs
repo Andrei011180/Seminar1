89adb319b6df134f35777863c3756bffe8b46b29
Andrei <reysavin@yandex.ru>
2023-09-20 22:21:08 +1000

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 2 . Напишите программу , которая на вход принимает два числа и выдает , какое число больше , а какое меньше .
+
+// Console.WriteLine(" Input first num ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(" Input second num");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if ( num1<num2)
+//      Console.WriteLine($"{num1} is less then the {num2}") ;
+// else 
+//      Console.WriteLine($"{num1} is greater then {num2}");
+
+// Задача 6 . Напишите програму , которая на вход принимает число и выдает , является ли число четным . 
+
+// Console.WriteLine( " Enter the number ") ;
+// int number = Convert.ToInt32(Console.ReadLine());
+//     if ( number % 2 ==0)
+//       Console.WriteLine($"The {number} an even number") ;
+//     else 
+//       Console.WriteLine($"The {number} is odd number");
+
+// Задача 4 . Напишите программу , которая на вход принимает число N , 
+// а на выходе показывает все четные числа от 1 до N
+
+
+    
+
+
+
+    
+        
+
+
+
